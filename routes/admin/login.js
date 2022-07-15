469eab3ba0aef8c4274d64a961cfb6d4dac05c1a
DamianBarzolaweb <damianbarzolaweb@gmail.com>
2022-07-15 03:25:57 -0300

    update

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,5 +8,27 @@ router.get('/', function(req, res, next) {
     layout:'admin/layout'
 });
 
+router.post('/', async function(req,res,next){
+    try{
+        console.log(req.body);
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+        var data = await usuariosModel.getUserAndPassword(usuario,password);
+
+        console.log(data);
+
+        if(data != undefined){
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/login',{
+                layout:'admin/layout',
+                error:true
+            })
+        }
+
+    }catch(error){
+        console.log(error)
+    }
+})
 
 module.exports = router;
