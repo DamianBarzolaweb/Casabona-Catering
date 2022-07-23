a81a25dffa090459095ce9c19aaffe12f8f530f3
DamianBarzolaweb <damianbarzolaweb@gmail.com>
2022-07-23 17:02:29 -0300

    update

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require ('express');
-const app = express ();
-const port = 4000;
+// const express = require ('express');
+// const app = express ();
+// const port = 4000;
 
-const popover = new bootstrap.Popover('.example-popover', {
-    container: 'body'
-  })
+// const popover = new bootstrap.Popover('.example-popover', {
+//     container: 'body'
+//   })
   
-  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+//   const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+//   const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
-app.get('/', (request, response) => {
-    response.send(<h1>hello world</h1>)
-})
+// app.get('/', (request, response) => {
+//     response.send(<h1>hello world</h1>)
+// })
 
-app.get
+// app.get
